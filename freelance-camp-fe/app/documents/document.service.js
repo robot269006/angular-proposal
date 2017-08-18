a7e0bc6541b64d2a71cc47a59acdcf25b12c9c75
robot269006 <robot269006@yahoo.co.jp>
2017-08-18 18:19:34 +0900

    Implementing angular 2 "Observables"
    1. import libraries -> http, response, headers, request options,
    observable libraries
    2. set connecting api url
    3. allow http library to be called when the access is made with api
    4. set observables -> returns an array of documents -> set with a
    function called “getDocuments”
    5. add error handler
    
    *Rxjs = use concept of “observables” = asynchronous data stream. image
    = it lets multiple event occur automatically

--- freelance-camp-fe/app/documents/document.service.js
+++ freelance-camp-fe/app/documents/document.service.js
@@ -5,15 +5,47 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
+var http_1 = require("@angular/http");
+var Rx_1 = require("rxjs/Rx");
 var DocumentService = (function () {
-    function DocumentService() {
+    //constructor is going to create connection with HTTP, since this api connection will be dependent on HTTP call
+    function DocumentService(
+        //private varible created, and the type is defined as "Http" -> Http library is called
+        http) {
+        this.http = http;
+        //api url
+        this.documentsUrl = 'http://localhost:3001/freelance_documents.json';
     }
+    //return an observable. Observable takes a type argument -> angled brackets. We want this observalbe to retun an array of documents
+    DocumentService.prototype.getDocuments = function () {
+        return this.http.get(this.documentsUrl)
+            .map(function (response) { return response.json(); }) //map a response to a response that we can actually work with
+            .catch(this.handleError);
+    };
+    DocumentService.prototype.handleError = function (error) {
+        // In a real world app, we might use a remote logging infrastructure
+        var errMsg;
+        if (error instanceof http_1.Response) {
+            var body = error.json() || '';
+            var err = body.error || JSON.stringify(body);
+            errMsg = error.status + " - " + (error.statusText || '') + " " + err;
+        }
+        else {
+            errMsg = error.message ? error.message : error.toString();
+        }
+        console.error(errMsg);
+        return Rx_1.Observable.throw(errMsg);
+    };
     return DocumentService;
 }());
 DocumentService = __decorate([
-    core_1.Injectable()
+    core_1.Injectable(),
+    __metadata("design:paramtypes", [http_1.Http])
 ], DocumentService);
 exports.DocumentService = DocumentService;
 //# sourceMappingURL=document.service.js.map
