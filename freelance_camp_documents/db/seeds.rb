596fef0b109fc2b91b97094b10a1b0fdf8896d75
robot269006 <robot269006@yahoo.co.jp>
2017-08-18 16:22:43 +0900

    Generating sample data in rails api
    1. create data sample in seeds.rb
    2. bundle exec rake db:setup
    3. rails s -p 3001 -> overrides default rails server settings -> since
    port 3000 is used by angular, it needs to be opened through a different
    port -> ultimately, it would need 3 separate servers running
    simultaneously
    4. try accessing on browser -> localhost:3001/freelance_documents ->
    shows created records on seeds.rb as objects (no view is applied, so it
    looks messy)

--- freelance_camp_documents/db/seeds.rb
+++ freelance_camp_documents/db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |d|
+  FreelanceDocument.create!(
+  title: "Document #{d}",
+  description: "Lorem ipsum dolor sit amet, quaeque graecis pri et. Has eu sale exerci, in pro vide ferri percipit. Mel ea diam nostrud reformidans, vis no animal repudiandae. Id sit suas mundi eligendi, ut tantas singulis mei.",
+  file_url: "http://google.com",
+  image_url: "http://google.com")
+end
+
+puts "10 document records are made"
